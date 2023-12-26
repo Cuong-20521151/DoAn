dd1cb13b82e7f31e42fe0289a965e03d038a72cf
Cuong <20521151@gm.uit.edu.vn>
2023-12-26 23:56:25 +0700

    sua_26_12_lan_1

--- server/Routers/Routers.js
+++ server/Routers/Routers.js
@@ -52,16 +52,7 @@ router.get('/getUser', async (req, res) => {
         res.status(500).json({message: error.message})
     }
 })
-//Get user by Id
-router.get('/getUser/:id', async (req, res) => {
-    try{
-        const data = await User.find();
-        res.json(data)
-    }
-    catch(error){
-        res.status(500).json({message: error.message})
-    }
-})
+
 //Post by user
 router.post('/Login', async (req, res) => {
     try{
@@ -136,6 +127,23 @@ router.patch('/update/:id', async (req, res) => {
         const options = { new: true };
 
         const result = await Dish.findByIdAndUpdate(
+            id, updatedData, options
+        )
+
+        res.send(result)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+// Update by User 
+router.patch('/updateUser/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = { new: true };
+
+        const result = await User.findByIdAndUpdate(
             id, updatedData, options
         )
 
