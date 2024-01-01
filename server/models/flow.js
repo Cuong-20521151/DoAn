4fcfadd488e32b11d839bc02fdc6960899bcaa3e
Cuong <20521151@gm.uit.edu.vn>
2024-01-01 21:28:46 +0700

    Cuong_sua

--- server/models/flow.js
+++ server/models/flow.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 const dataSchema = new mongoose.Schema({
     
@@ -9,16 +8,5 @@ const dataSchema = new mongoose.Schema({
         required: true, type: String
     },
 })
-=======
-const mongoose = require('mongoose');
-const dataSchema = new mongoose.Schema({
-    
-    user_flow: {
-        required: true, type: String
-    },
-    userId: {
-        required: true, type: String
-    },
-})
->>>>>>> 8d2b362f2620f144394301968dda8293a456c011
+
 module.exports = mongoose.model('flow', dataSchema)
